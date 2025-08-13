ee65da309d9475bf87f8acf5bdc781504c15f4c2
unknown <rajabpour4097@yahoo.com>
2025-08-13 20:24:44 +0330

    Improved save file for each day

--- save_file.py
+++ save_file.py
@@ -1,18 +1,17 @@
+from datetime import datetime
 from colorama import init, Fore
 
 # راه‌اندازی colorama
 init(autoreset=True)
-
-# تابع لاگ با رنگ و ذخیره‌سازی در فایل
-LOG_FILE = 'swing_logs.txt'
 
 def log(msg, level='info', color=None, save_to_file=True):
     color_prefix = getattr(Fore, color.upper(), '') if color else ''
     print(f"{color_prefix}{msg}")
 
     if save_to_file:
+        log_filename = f"swing_logs_{datetime.now().strftime('%Y-%m-%d')}.txt"
         try:
-            with open(LOG_FILE, 'a', encoding='utf-8') as f:
+            with open(log_filename, 'a', encoding='utf-8') as f:
                 f.write(f"{msg}\n")
         except Exception as e:
             print(f"خطا در ذخیره لاگ: {e}")
