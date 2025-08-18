d06f9a4c9d24db52500a47123440235699004b00
rajabpour4097 <rajabpour4097@gmail.com>
2025-08-18 21:40:51 +0330

    change to full-time

--- metatrader5_config.py
+++ metatrader5_config.py
@@ -52,7 +52,7 @@ MT5_CONFIG = {
     'max_spread': 3.0,
     'min_balance': 100,
     'max_daily_trades': 10,
-    'trading_hours': IRAN_ACTIVE_HOURS,
+    'trading_hours': FULL_TIME_IRAN,
 }
 
 # تنظیمات استراتژی
